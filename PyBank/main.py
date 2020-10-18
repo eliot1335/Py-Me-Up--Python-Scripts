3189b972b18dc8c63f0f29b5a9eb9796cfbf9aaa
Eliot Chern <eliotchern@gmail.com>
2020-10-18 10:44:10 -0700

    Get ready to decompose the task

---
+++ PyBank/main.py
@@ -0,0 +1,16 @@
+# Create a script with:
+    # The total number of months included in the dataset
+    # The net total amount of "Profit/Losses" over the entire period
+    # The average of the changes in "Profit/Losses" over the entire period
+    # The greatest increase in profits (date and amount) over the entire period
+    # The greatest decrease in losses (date and amount) over the entire period
+# Print analysis and export a text file as report
+
+
+
+
+
+
+
+
+
