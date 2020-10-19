547b9b0eac0b8b337338aa6d311b187b1181b08f
Eliot Chern <eliotchern@gmail.com>
2020-10-19 10:38:36 -0700

    setting up ground work

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,12 +4,28 @@
     # The average of the changes in "Profit/Losses" over the entire period
     # The greatest increase in profits (date and amount) over the entire period
     # The greatest decrease in losses (date and amount) over the entire period
-# Print analysis and export a text file as report
+    # Print analysis and export a text file as report
 
+# load raw data
+import os
+import csv
 
+# Set path
+csvpath = os.path.join("Resources", "budget_data.csv")
+# Open file
+with open(csvpath) as csvfile:
 
-
-
+# loop through every row in csv but skip the header
+# contain the loop number in a variable and print in analysis later
+"""
+print("Financial Analysis")
+print("----------------------------")
+print("Total Months: 86")
+print("Total: $38382578")
+print("Average  Change: $-2315.12")
+print("Greatest Increase in Profits: Feb-2012 ($1926159")
+print("Greatest Decrease in Profits: Sep-2013 ($-2196167)")
+"""
 
 
 
